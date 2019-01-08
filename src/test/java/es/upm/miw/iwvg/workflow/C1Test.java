f9efc4e2eed1faab0e6261291b4755e05396865c
jbernal <miw@etsisi.upm.es>
2019-01-08 18:13:41 +0100

    update C1, add m2

--- src/test/java/es/upm/miw/iwvg/workflow/C1Test.java
+++ src/test/java/es/upm/miw/iwvg/workflow/C1Test.java
@@ -5,8 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class C1Test {
+
     @Test
     void testM1() {
         assertEquals("C1::m1", new C1().m1());
+    }
+
+    @Test
+    void testM2() {
+        assertEquals("C1::m2", new C1().m2());
     }
 }
