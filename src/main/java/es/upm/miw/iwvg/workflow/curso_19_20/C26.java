51634183346e0a54a62dacea0408b1b9c17616be
pablo perez <pperezj@alumnos.unex.es>
2019-09-17 12:32:57 +0200

    add C26

--- src/main/java/es/upm/miw/iwvg/workflow/curso_19_20/C26.java
+++ src/main/java/es/upm/miw/iwvg/workflow/curso_19_20/C26.java
@@ -1,4 +1,12 @@
 package es.upm.miw.iwvg.workflow.curso_19_20;
 
 public class C26 {
+
+    public String m2(){
+        return "Devolviendo String m2";
+    }
+
+    public String m3(){
+        return "Devolver String m3";
+    }
 }
